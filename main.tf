8836d6629faf8badc47d674703040038752d2f5f
Jorge Carmine <george_gamez@hotmail.com>
2023-01-15 09:12:32 -0600

    Add application load balancer

--- main.tf
+++ main.tf
@@ -2,15 +2,27 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "carmine_server" {
+data "aws_subnet" "az_a" {
+  availability_zone = "us-east-1a"
+}
+
+data "aws_subnet" "az_b" {
+  availability_zone = "us-east-1b"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_instance" "carmine_server_1" {
   ami           = "ami-06878d265978313ca"
   instance_type = "t2.micro"
-
-  vpc_security_group_ids = [aws_security_group.carmine_security_group.id]
+  subnet_id = data.aws_subnet.az_a.id
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
 
   user_data = <<-EOF
                 #!/bin/bash
-                echo "Hola Terraformers!" > index.html
+                echo "Hola JorgeCarmine! Soy server 1" > index.html
                 nohup busybox httpd -f -p 8080
                 EOF
 
@@ -19,13 +31,98 @@ resource "aws_instance" "carmine_server" {
   }
 }
 
-resource "aws_security_group" "carmine_security_group" {
+resource "aws_instance" "carmine_server_2" {
+  ami                    = "ami-06878d265978313ca"
+  instance_type          = "t2.micro"
+  subnet_id = data.aws_subnet.az_b.id
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hola JorgeCarmine! Soy server 2" > index.html
+                nohup busybox httpd -f -p 8080
+                EOF
+
+  tags = {
+    Name = "Second server"
+  }
+}
+
+# Security group de las EC2s
+resource "aws_security_group" "ec2_security_group" {
   name = "main-server"
   ingress {
-    cidr_blocks = ["0.0.0.0/0"]
+    # cidr_blocks = [ "0.0.0.0/0" ]
+    security_groups = [aws_security_group.carmine_alb_sg.id]
     description = "Public access from 8080 port"
     from_port   = 8080
     to_port     = 8080
     protocol    = "TCP"
+  }
+}
+
+
+resource "aws_lb" "carmine_alb" {
+  load_balancer_type = "application"
+  name = "carmine-alb"
+  security_groups = [aws_security_group.carmine_alb_sg.id]
+  subnets = [ data.aws_subnet.az_a.id, data.aws_subnet.az_b.id ]
+}
+
+# Security group del load balancer
+resource "aws_security_group" "carmine_alb_sg" {
+  name = "carmine-alb-sg"
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Public access from port 80"
+    from_port = 80
+    to_port = 80
+    protocol = "TCP"
+  }
+
+  egress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "Acceso al puerto 8080 de las EC2s"
+    from_port = 8080
+    to_port = 8080
+    protocol = "TCP"
+  }
+} 
+
+resource "aws_lb_target_group" "this" {
+  name = "carmine-alb-target-group"
+  port = 80
+  vpc_id = data.aws_vpc.default.id
+  protocol = "HTTP"
+  
+  health_check {
+    enabled = true
+    matcher = "200"
+    path = "/"
+    port = "8080"
+    protocol = "HTTP"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "server_1_attachment" {
+  target_group_arn = aws_lb_target_group.this.arn
+  target_id = aws_instance.carmine_server_1.id
+  port = 8080
+}
+
+resource "aws_lb_target_group_attachment" "server_2_attachment" {
+  target_group_arn = aws_lb_target_group.this.arn
+  target_id = aws_instance.carmine_server_2.id
+  port = 8080
+}
+
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.carmine_alb.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.this.arn
+    type = "forward"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,25 @@
-output "public_dns" {
+output "public_dns_server_1" {
     description = "Public DNS"
-    value = "http://${aws_instance.carmine_server.public_dns}"
+    value = "http://${aws_instance.carmine_server_1.public_dns}:8080"
 }
 
-output "public_ip" {
+output "public_ip_server_1" {
     description = "Public IP"
-    value = aws_instance.carmine_server.public_ip
+    value = aws_instance.carmine_server_1.public_ip
+}
+
+
+output "public_dns_server_2" {
+    description = "Public DNS"
+    value = "http://${aws_instance.carmine_server_2.public_dns}:8080"
+}
+
+output "public_ip_server_2" {
+    description = "Public IP"
+    value = aws_instance.carmine_server_2.public_ip
+}
+
+output "dns_load_banacer" {
+  description = "DNS público del load balancer"
+  value = "https://${aws_lb.carmine_alb.dns_name}"
 }
